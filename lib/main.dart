c261d613cd0da64e05f114d4788f15228a0968b5
kimyejin <7602kyj@e-mirim.hs.kr>
2023-02-19 17:48:15 +0900

    pomodoro : pomodoro timer 제작

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:smallscaleflutterproject/ui_challenge/screen/card_list_screen.dart';
+import 'package:smallscaleflutterproject/pomodoro/screen/pomodoro_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,9 +16,17 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        backgroundColor: const Color(0xFFE7626C),
+        textTheme: const TextTheme(
+          headline1: TextStyle(
+            color: Color(0xFF232B55),
+          ),
+        ),
+        cardColor: Colors.white,
       ),
       routes: {
         '/cardList': (context) => CardListScreen(),
+        '/pomodoro': (context) => PomodoroScreen(),
       },
       debugShowCheckedModeBanner: false,
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -55,6 +64,12 @@ class _MyHomePageState extends State<MyHomePage> {
               Navigator.pushNamed(context, '/cardList');
             },
             child: Text('UI CHALLENGE'),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/pomodoro');
+            },
+            child: Text('POMODORO'),
           ),
         ],
       ),
