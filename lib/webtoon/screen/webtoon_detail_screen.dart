c062dabba509a6948e69b0cd79a057aaffc5f216
kimyejin <7602kyj@e-mirim.hs.kr>
2023-03-05 18:22:12 +0900

    webtoon: 웹툰 리스트 -> 웹툰 상세 이동 작업

---
+++ lib/webtoon/screen/webtoon_detail_screen.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:smallscaleflutterproject/webtoon/models/webtoon_model.dart';
+
+class WebtoonDetailScreen extends StatelessWidget {
+  final WebtoonModel webtoon;
+
+  WebtoonDetailScreen({
+    Key? key,
+    required this.webtoon,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.green,
+        title: Text(
+          '${webtoon.title}',
+          style: TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+        elevation: 0,
+      ),
+      body: Column(
+        children: [
+          SizedBox(height: 50),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Hero( /// 화면을 넘나드는 애니메이션 효과를 주는 widget
+                tag: webtoon.id,
+                child: Container(
+                  width: 250,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.4),
+                        offset: const Offset(8, 10),
+                        blurRadius: 15,
+                      ),
+                    ],
+                  ),
+                  clipBehavior: Clip.hardEdge,
+                  child: Image.network(
+                    webtoon.thumb,
+                    headers: const {
+                      "User-Agent":
+                          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/webtoon/screen/webtoon_list_screen.dart
+++ lib/webtoon/screen/webtoon_list_screen.dart
@@ -1,16 +1,31 @@
+import 'dart:core';
 import 'package:flutter/material.dart';
+import 'package:flutter/src/painting/box_shadow.dart';
 import 'package:smallscaleflutterproject/webtoon/services/api_service.dart';
+import 'package:smallscaleflutterproject/webtoon/models/webtoon_model.dart';
+import 'package:smallscaleflutterproject/webtoon/widget/webtoon_card_widget.dart';
 
 class WebtoonListScreen extends StatelessWidget {
-  const WebtoonListScreen({Key? key}) : super(key: key);  /// [Key]는 위젯에 'ID' 라고 생각
+  WebtoonListScreen({Key? key}) : super(key: key);  /// [Key]는 위젯에 'ID' 라고 생각
 
-  void getTodayToons() {
-    ApiService.getTodaysToons();
-  }
+  /// statefulWidget을 상속 받아 setState() 해주기보다
+  /// statelessWidget을 상속 받아 FutureBuilder widget으로 데이터를 받아오는 것이 일반화
+
+  final Future<List<WebtoonModel>> _webtoonList = ApiService.getTodaysToons();
+
+  /// 1. StatefulWidget을 상속받는 경우, setState를 통해 webtoonsList load
+  // void _waitForWebtoons() {
+  //   ApiService.getTodaysToons().then((webtoonList) {
+  //     _webtoonList = webtoonList;
+  //
+  //     setState(() {   /// [setState()] 는 활용을 지양함.
+  //       _isLoading = false;
+  //     });
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
-    getTodayToons();
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -25,6 +40,40 @@ class WebtoonListScreen extends StatelessWidget {
         ),
         elevation: 0,
       ),
+      body: FutureBuilder(
+        future: _webtoonList,
+        builder: (context, AsyncSnapshot snapshot) {
+          print(snapshot.hasData);
+          if (snapshot.hasData) {
+            return Column(
+              children: [
+                SizedBox(height: 50),
+                Expanded( /// 남는 영역을 자식 widget으로 채움.
+                  child: _webtoonListWidget(snapshot.data!),
+                ),
+              ],
+            );
+          };
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _webtoonListWidget(webtoonList) {
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      itemCount: webtoonList.length,  /// [hasData]가 [true]일 경우에만 반환하므로 [null] 보증 (!)
+      padding: EdgeInsets.symmetric(horizontal: 15.0),
+      itemBuilder: (context, index) {
+        var webtoon = webtoonList[index];
+        return WebtoonCardWidget(webtoon: webtoon);
+      },
+      separatorBuilder: (context, index) {
+        return SizedBox(width: 40);
+      },
     );
   }
 }
