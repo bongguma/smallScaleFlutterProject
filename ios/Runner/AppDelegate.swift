42226c42d93540be77e5e4a6d7a9621671f8506e
kimyejin <7602yejin@gmail.com>
2023-03-09 17:16:23 +0900

    iOS naver login 연동

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import NaverThirdPartyLogin
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -9,5 +10,18 @@ import Flutter
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+      var applicationResult = false
+      if (!applicationResult) {
+         applicationResult = NaverThirdPartyLoginConnection.getSharedInstance().application(app, open: url, options: options)
+      }
+      // if you use other application url process, please add code here.
+
+      if (!applicationResult) {
+         applicationResult = super.application(app, open: url, options: options)
+      }
+      return applicationResult
   }
 }
